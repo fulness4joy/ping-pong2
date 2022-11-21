401976b06af58667c939fb7860e845c48c5cdc51
fulness4joy <117214478+fulness4joy@users.noreply.github.com>
2022-11-21 17:22:21 +0200

    Add files via upload
    Додав перший варіант гри і картинки

---
+++ ping_pong0.py
@@ -0,0 +1,46 @@
+from pygame import *
+'''Необхідні класи'''
+ 
+# клас-батько для спрайтів
+class GameSprite(sprite.Sprite):
+    #конструктор класу
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        # кожен спрайт повинен зберігати властивість image - зображення
+        self.image = transform.scale(image.load(player_image), (wight, height)) #разом 55,55 - параметри
+        self.speed = player_speed
+        # кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+#ігрова сцена:
+back = (200, 255, 255)  #колір фону (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+ 
+#прапорці, що відповідають за стан гри
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+ 
+font.init()
+
+ 
+speed_x = 3
+speed_y = 3
+ 
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    display.update()
+    clock.tick(FPS)
